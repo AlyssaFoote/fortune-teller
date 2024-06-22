adcb144a5bb2e3f9a459bf35e3b2ddbf5b8c77ef
Alyssa Foote <alyssa.foote.22@gmail.com>
2024-06-22 10:43:12 -0700

    Fixed the bug in the fortune() function
    In my original code, return statements were not necessary as I didn't need to return the value, I just needed to update the text content.

--- script.js
+++ script.js
@@ -1,36 +1,20 @@
-//Fortune teller script
-/*
-
-01. User clicks the button to call the function
-02. Once the button is clicked, a randomly generated fortune appears
-*/
 let myButton = document.querySelector("button");
-let myPrediction = document.querySelector("fortune_told");
-
-// Create the function
-// 
+let myPrediction = document.querySelector("#fortune_told");
 
 function fortune() {
-
     let fortuneNum = Math.floor(Math.random() * 3);
 
     if (fortuneNum === 0) {
-        return myPrediction.innerText("You are a cutie pie");
+        myPrediction.textContent = "You are a cutie pie";
     } else if (fortuneNum === 1) {
-        return myPrediction.innerText("U R A QT PIE");
+        myPrediction.textContent = "U R A QT PIE";
     } else if (fortuneNum === 2) {
-        return myPrediction.innerText("U R A SUPER QT PIE FOREVER!!!");
+        myPrediction.textContent = "U R A SUPER QT PIE FOREVER!!!";
     } else {
-        return myPrediction.innerText("You are a technical difficulty!! But still cute");
+        myPrediction.textContent = "You are a technical difficulty!! But still cute";
     }
-};
-
-// Create the button.onclick = () => {fortune()};
+}
 
 myButton.onclick = () => {
     fortune();
 }
-
-prediction.addEventListener("click", function () {
-    prediction.innerText = "U R A QT PIE";
-});
