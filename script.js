d7d14a0c18b9568ac725c3ec1437f5c1437a21e7
Alyssa Foote <alyssa.foote.22@gmail.com>
2024-06-22 10:31:19 -0700

    Add basic logical structure and functionality

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 //Fortune teller script
-
-//When the user presses the button, the script will procure a randomly generated fortune.
-
-
 /*
 
 01. User clicks the button to call the function
-02. 
+02. Once the button is clicked, a randomly generated fortune appears
 */
-const prediction = document.getElementById("fortune_told");
+let myButton = document.querySelector("button");
+let myPrediction = document.querySelector("fortune_told");
+
+// Create the function
+// 
+
+function fortune() {
+
+    let fortuneNum = Math.floor(Math.random() * 3);
+
+    if (fortuneNum === 0) {
+        return myPrediction.innerText("You are a cutie pie");
+    } else if (fortuneNum === 1) {
+        return myPrediction.innerText("U R A QT PIE");
+    } else if (fortuneNum === 2) {
+        return myPrediction.innerText("U R A SUPER QT PIE FOREVER!!!");
+    } else {
+        return myPrediction.innerText("You are a technical difficulty!! But still cute");
+    }
+};
+
+// Create the button.onclick = () => {fortune()};
+
+myButton.onclick = () => {
+    fortune();
+}
 
 prediction.addEventListener("click", function () {
-    prediction.textContent = "U R A QT PIE";
+    prediction.innerText = "U R A QT PIE";
 });
